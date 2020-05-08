09cb6a5f6337303b86e6609ab44099b13837465e
mywork <abdala@gmail.com>
2020-05-08 14:45:11 -0500

    api message updated

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ let express = require('express')
 let router = express.Router()
 
 router.get('/', function(rerq, res, next){
-    res.json({ 'message': 'hello ITEC 2560 Web programmers'})
+    res.json({ 'message': 'hello SMART ITEC 2560 Web programmers'})
 })
 
 module.exports = router
